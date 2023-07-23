7df5a5a18122e87e1f317171ab8d7a06fc8a3146
Ron <ramayer@gmail.com>
2023-07-23 15:52:02 -0700

    added help file

---
+++ sv/src/routes/s/Help.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let help = false;
+</script>
+
+{#if help}
+  <section
+    id="help"
+    class="block h-full"
+    on:click={() => {
+      help = false;
+    }}
+    on:keydown={() => {
+      help = false;
+    }}
+  >
+    HELP!
+    {help}
+    <button
+      on:click={() => {
+        help = false;
+      }}>hi</button
+    >
+  </section>
+{/if}
+
+<style>
+  #help {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 2000;
+    justify-content: center;
+    align-items: center;
+    opacity: 1;
+    background: rgba(0, 0, 0, 0.8);
+  }
+</style>
